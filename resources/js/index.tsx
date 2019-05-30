c1864d108143acf3ad1d58f7fc4e9572ee9d894c
Hiroki 111 <mitsuke1116@gmail.com>
2019-05-30 07:08:11 +0200

    Removed an redundant line

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -4,8 +4,6 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './components/store';
 
-ReactDOM.render(<App/>, document.getElementById('react-app'));
-
 ReactDOM.render(<Provider store={store} >
 	<App />
 </Provider>, document.getElementById('react-app'));
