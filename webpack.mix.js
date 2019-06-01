95d84c37de13753edb9142cc08f981c12e7763fb
Hiroki 111 <mitsuke1116@gmail.com>
2019-06-01 08:57:42 +0200

    Introduced browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,45 +5,46 @@ const Fiber = require('fibers');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 mix.webpackConfig({
-		devtool: "inline-source-map",
-		entry: './resources/js/index.tsx',
-		resolve: {
-			extensions: ['.*','.js', '.jsx', '.ts', '.tsx'],
-		},
-		output: {
-			filename: 'app.js',
-			path: path.resolve(__dirname, 'public/js')
-		},
-		module: {
-			rules: [{
-				test: /\.(js|jsx)?$/,
-				loader: "babel-loader",
-				exclude: /node_modules/
+	devtool: "inline-source-map",
+	entry: './resources/js/index.tsx',
+	resolve: {
+		extensions: ['.*', '.js', '.jsx', '.ts', '.tsx'],
+	},
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'public/js')
+	},
+	module: {
+		rules: [{
+			test: /\.(js|jsx)?$/,
+			loader: "babel-loader",
+			exclude: /node_modules/
+		}, {
+			test: /\.(ts|tsx)?$/,
+			use: [{ loader: 'ts-loader' }],
+			exclude: /node_modules|\.d\.ts$/
+		}, {
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader"
 			}, {
-				test: /\.(ts|tsx)?$/,
-				use:[{loader: 'ts-loader'}],
-				exclude: /node_modules|\.d\.ts$/
+				loader: "css-loader"
 			}, {
-				test: /\.scss$/,
-				use: [{
-					loader: "style-loader"
-				}, {
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader",
-					options: {
-						implementation: require("sass"),
-						fiber: Fiber
-					}
-				}]
+				loader: "sass-loader",
+				options: {
+					implementation: require("sass"),
+					fiber: Fiber
+				}
 			}]
-		},
-		plugins: [
-			new MiniCssExtractPlugin({
-				filename: "[name].css",
-				chunkFilename: "[id].css"
-			}),
-			new CleanWebpackPlugin(),
-		]
-	}).react('resources/js/index.tsx', 'public/js')
-	.sass('resources/sass/app.scss', 'public/css');
+		}]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		new CleanWebpackPlugin(),
+	]
+}).react('resources/js/index.tsx', 'public/js')
+	.sass('resources/sass/app.scss', 'public/css')
+	.browserSync('event-management-test');
