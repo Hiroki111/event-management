aa6bf3a262c7ab332153c15828fedf087a13a027
Hiroki 111 <mitsuke1116@gmail.com>
2019-06-02 09:19:18 +0200

    Updated Navigation

--- resources/js/components/Navigation/index.tsx
+++ resources/js/components/Navigation/index.tsx
@@ -1,7 +1,64 @@
-import * as React from "react";
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import EventIcon from '@material-ui/icons/Event';
+import BusinessIcon from '@material-ui/icons/Business';
+import MicIcon from '@material-ui/icons/Mic';
+import PeopleIcon from '@material-ui/icons/People';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import LogOffIcon from '@material-ui/icons/PowerSettingsNew';
+import Typography from '@material-ui/core/Typography';
+
+interface TabContainerI {
+    children: any,
+};
+
+const TabContainer = (props: TabContainerI) => {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
 
 const Navigation = () => {
-    return <div>Navigation</div>;
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: any) => {
+        setValue(newValue);
+    }
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons="on"
+                    indicatorColor="primary"
+                    textColor="primary"
+                >
+                    <Tab label="EVENT" icon={<EventIcon />} />
+                    <Tab label="ORGANIZERS" icon={<BusinessIcon />} />
+                    <Tab label="EQUIPMENT" icon={<MicIcon />} />
+                    <Tab label="MEMBERS" icon={<PeopleIcon />} />
+                    <Tab label="ROLES" icon={<PeopleOutlineIcon />} />
+                    <Tab label="LOG OFF" icon={<LogOffIcon />} />
+                </Tabs>
+            </AppBar>
+        </div>
+    );
 }
 
 export default Navigation;
