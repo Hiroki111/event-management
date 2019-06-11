1c57dbfdd8683bb1d56441b1aa8ea54a452bcc55
Hiroki 111 <mitsuke1116@gmail.com>
2019-06-11 07:53:18 +0200

    Put FullCalendar into event

--- resources/js/components/Events/index.tsx
+++ resources/js/components/Events/index.tsx
@@ -1,7 +1,23 @@
 import * as React from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import './styles.scss';
 
 const Events = () => {
-    return <div>Events</div>;
+    return (
+        <div id="event-wrapper">
+            <div id="calendar-wrapper">
+                <FullCalendar
+                    defaultView="dayGridMonth"
+                    plugins={[dayGridPlugin]}
+                    events={[
+                        { title: 'event 1', date: '2019-06-01' },
+                        { title: 'event 2', date: '2019-06-02' }
+                    ]} />
+            </div>
+            <div id="event-menu-wrapper"></div>
+        </div>
+    );
 }
 
 export default Events;
